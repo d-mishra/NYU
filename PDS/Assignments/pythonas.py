9921dfdbefb65aa8e57962846e2db016e3737169
DM <dm3501.stern.nyu.edu>
2014-07-10 10:57:21 -0400

    updating assignment 1

--- PDS/Assignments/pythonas.py
+++ PDS/Assignments/pythonas.py
@@ -1,5 +1,10 @@
+"""
+@author: DM
+
+"""
 import matplotlib.pyplot as plt
 import numpy as np
+
 
 survey_file = open ("survey_anon.txt","r")
 
@@ -17,6 +22,13 @@ for line in survey_file:
 	columns = line.strip().split("\t")
 	
 #Unix
+ 
+ #I dont even understand the question
+ #I have no experience working in a terminal
+ #I have issued a few commands in a terminal based on given instructions
+ #I have written simple terminal commands or done some system work on the term
+ #I am a Unix hacker
+ 
 	if columns[1] == "I dont even understand the question":
 		unix_count[0] = unix_count[0] + 1
 	elif columns[1] == "I have no experience working in a terminal":
@@ -33,6 +45,12 @@ for line in survey_file:
 
 # Database
 
+#I am a database hacker
+#I can write very complex queries when needed
+#I can write simple queries and issue them to a database
+#I have issued simple queries to a relational database based on given instructions
+#I have never directly accessed a database
+
 	if columns[2] == "I have never directly accessed a database":
 		db_count[0] = db_count[0] + 1
 	elif columns[2] == "I have issued simple queries to a relational database based on given instructions":
@@ -46,6 +64,13 @@ for line in survey_file:
 
 
 # programming
+
+#I am a hacker or have  senior-level programming experience
+#I can write complex programs, am familiar with programming design patterns, software testing, system design, and algorithms
+#I can write simple programs to accomplish tasks I encounter
+#I have written simple programs, based on instructions or a tutorial
+#I have never programmed before.
+
 	if columns[3] == "I have never programmed before.":
 		prog_count[0] = prog_count[0] + 1
 	elif columns[3] == "\"I have written simple programs, based on instructions or a tutorial\"":
@@ -57,21 +82,6 @@ for line in survey_file:
 	elif columns[3] == "I am a hacker or have  senior-level programming experience":
 		prog_count[4] = prog_count[4] + 1
 	
-
-#Database
-#I am a database hacker
-#I can write very complex queries when needed
-#I can write simple queries and issue them to a database
-#I have issued simple queries to a relational database based on given instructions
-#I have never directly accessed a database
-
-#Programming
-#I am a hacker or have  senior-level programming experience
-#I can write complex programs, am familiar with programming design patterns, software testing, system design, and algorithms
-#I can write simple programs to accomplish tasks I encounter
-#I have written simple programs, based on instructions or a tutorial
-#I have never programmed before.
-
 
 #Compute the average score 
 
@@ -102,21 +112,21 @@ print "The lowest overall skill level is in %s " %class_min
 unix_xval = [1, 2, 3, 4, 5]
 #to substitute the strings describing the skill level with a numeric value
 #starting from the lowest and going to the highest
-#plt.plot(unix_xval, unix_vals)
+
 plt.plot(unix_xval,unix_count)
 plt.show()
 
 #2.Database
-#db_vals = [3, 4, 5, 4, 3] #change to db1_count...
+
 db_xval = [1, 2, 3, 4, 5]
-#plt.plot(db_xval, db_vals)
+
 plt.plot(db_xval, db_count)
 plt.show()
 
 #2.Programming
-#pro_vals = [10, 9, 8, 5, 7] #change to pro1_count...
+
 prog_xval = [1, 2, 3, 4, 5]
-#plt.plot(pro_xval, pro_vals)
+
 plt.plot(prog_xval, prog_count)
 plt.show()
 
@@ -131,12 +141,6 @@ plt.legend(['Unix', 'Database', 'Programming'])
 plt.show()
 
 #4.bar plot
-#plt.bar(unix_xval,unix_count,db_xval,db_count,prog_xval,prog_count)
-#ax.set_ylabel('Scores')
-#ax.set_xticklabels( ('1', '2', '3', '4', '5') )
-#plt.show()
-
-# bar chart
 
 offset = 0.3
 plt.bar(unix_xval,unix_count , offset, color='r',align='edge')
