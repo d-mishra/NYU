de79a8ee628908e51af5dfac28ff2909d0a89733
DM <dm3501.stern.nyu.edu>
2014-07-20 00:10:02 -0400

    Updated correct formula for distance

--- PDS/Project/ECommerce_Project_With_Eucl.py
+++ PDS/Project/ECommerce_Project_With_Eucl.py
@@ -117,7 +117,7 @@ for sliceStart in range(0,len(etsyCorpus),1):
         while(etsyResults[secStart]!=etsyResults[sliceStart]):
             SecAr=[int(etsyResults[secStart]["listing_active_count"]),int(etsyResults[secStart]["num_favorers"])]
 #            EudDist = la.norm(PrimAr-SecAr)
-            EudDist = math.sqrt((PrimAr[0]-SecAr[0])^2+((PrimAr[1]-SecAr[1])^2))
+            EudDist = ((PrimAr[0]-SecAr[0])**2+((PrimAr[1]-SecAr[1])**2))**0.05
             Doc_similarities.update={etsyResults["shop_name"]:EudDist}
         # sort the dictionary to get lest distance documents
         print Doc_similarities
